a99058f77d1511d9ec5b8915dabe5d1464267f87
im-mayur27 <mayurkeshari45@gmail.com>
2025-04-23 11:33:49 +0530

    add about and popularareas

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,14 +1,41 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import aboutimg from '../assets/Images/about.jpg'
-import {  useDarkMode } from '../Components/DarkModeContext'
+import { useDarkMode } from '../Components/DarkModeContext'
 import Aos from 'aos'
 import 'aos/dist/aos.css'
 
 const About = () => {
-  
+  useEffect(() => {
+    Aos.init({
+      offset: 200,
+      duration: 800,
+      easing: 'ease-in-sine',
+      delay: 100,
+    })
+  }, [])
+  const { darkMode, toggleDarkMode } = useDarkMode();
   return (
     <>
-    
+      <section id="about" className={`${darkMode ? 'dark bg-black' : 'light bg-white'} w-full m-auto lg:px-40 px-10
+       py-20 grid lg:grid-cols-2 grid-cols-1 justify-center items-center gap-10`}>
+        <div>
+          <img data-aos="zoom-in" src={aboutimg} alt="about-img" className='rounded-2xl lg:w-[500px] lg:h-[600px]' />
+        </div>
+
+        <div className='flex flex-col justify-center items-start gap-8'>
+             <h1 data-aos="zoom-in" className='text-red-500 dark:text-white font-medium'>WHO WE ARE ?</h1>
+             <h1 data-aos="zoom-in" data-aos-delay="200" className='text-black text-[40px] font-semibold leading-10 dark:text-white'>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, enim!</h1>
+
+             <p data-aos="zoom-in" data-aos-delay="400" className='text-xl text-gray-600 dark:text-white text-justify'>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum hic animi ex eos nemo, labore dicta!
+               Nemo quos at aperiam consequatur sit, commodi qui sunt.</p>  
+
+            <button data-aos="zoom-in" data-aos-delay="600" className='bg-red-600 dark:bg-red-700 hover:bg-black dark:hover:bg-white
+             dark:hover:text-black text-lg p-4 text-white font-semibold rounded-xl
+             cursor-pointer transform hover:scale-105 transition-transform-duration-300'>READ MORE</button>
+        </div>
+      </section>
     </>
   )
 }
--- src/sections/Popularareas.jsx
+++ src/sections/Popularareas.jsx
@@ -1,8 +1,42 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDarkMode } from '../Components/DarkModeContext'
+import area1 from '../assets/Images/area1.jpg'
+import area2 from '../assets/Images/area2.jpg'
+import area3 from '../assets/Images/area3.jpg'
+import Aos from 'aos'
+import 'aos/dist/aos.css'
 
 const Popularareas = () => {
+  useEffect(() => {
+    Aos.init({
+      offset: 200,
+      duration: 800,
+      easing: 'ease-in-sine',
+      delay: 100,
+    });
+  }, [])
+
+  const { darkMode, toggleDarkMode } = useDarkMode();
   return (
-    <div>Popularareas</div>
+    <>
+      <div className={`${darkMode ? 'dark bg-black' : 'light bg-transparent'}`}>
+        <section className={`${darkMode ? 'dark bg-gray-800' : 'light bg-red-100'} lg:w-[90%]
+       w-full h-fit bg-cover bg-center m-auto rounded-xl flex justify-center flex-col items-center lg:px-20 px-6 py-20 gap-20`}>
+          <div id="top" className='w-full grid lg:grid-cols-3 grid-cols-1 justify-center items-center gap-8'>
+            <div>
+              <h1 data-aos="zoom-in" className='text-red-500 dark:text-white font-medium'>POPULAR AREAS</h1>
+              <h1 data-aos="zoom-in" className='text-black text-[40px] font-semibold leading-10 mt-4 dark:text-white'>Explore Most <br></br>POPULAR AREAS</h1>
+            </div>
+
+            <div className='grid lg:grid-cols-3 col-span-2 grid-cols-1 justify-center items-center gap-6'>
+                 <div data-aos="zoom-in" data-aos-delay="400" style={{backgroundImage:`url(${area1})`}} className='h-[400px] bg-cover bg-center rounded-xl'></div>
+                 <div data-aos="zoom-in" data-aos-delay="600" style={{backgroundImage:`url(${area2})`}} className='h-[400px] bg-cover bg-center rounded-xl'></div>
+                 <div data-aos="zoom-in" data-aos-delay="800" style={{backgroundImage:`url(${area3})`}} className='h-[400px] bg-cover bg-center rounded-xl'></div>
+            </div>
+          </div>
+        </section>
+      </div>
+    </>
   )
 }
 
