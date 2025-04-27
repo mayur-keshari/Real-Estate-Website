67a6387c05d85788f20d377dd9b3212ad7f0d1cf
im-mayur27 <mayurkeshari45@gmail.com>
2025-04-27 16:18:47 +0530

    file complete

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { useDarkMode } from './DarkModeContext'
 import {Link} from 'react-scroll'
-import {FaMarker, FaBars, FaPhoneAlt, FaUserCircle}  from 'react-icons/fa'
+import {FaCircle, FaBars, FaPhoneAlt, FaUserCircle}  from 'react-icons/fa'
 import logo from '../assets/Images/logo.png'
 
 const Header = () => {   
@@ -22,24 +22,69 @@ const Header = () => {
         link:"home", path:"home"
       },
       {
-        link:"About", path:"about"
+        link:"about", path:"about"
       },
       {
-        link:"Properties", path:"properties"
+        link:"properties", path:"properties"
       },
       {
         link:"services", path:"services"
       },
       {
-        link:"Testimonials", path:"testimonials"
+        link:"testimonials", path:"testimonials"
       },
       {
-        link:"Contact", path:"contact"
+        link:"contact", path:"contact"
       },
     ]
   return (
-    <div></div>
+    <nav className={`${darkMode ? 'dark bg-black' : 'light bg-[#f3f3f3]'} flex justify-between items-center gap-4 lg:px-20 px-4 py-3 sticky top-0 z-30`}>
+      <div id="logo">
+        <img src={logo} alt="company-logo" className='lg:w-[150px] w-[120px] dark:invert'/>
+      </div>
+
+      <ul className='lg:flex justify-center items-center gap-8 hidden'>
+        {
+          navitems.map(({link,path})=>(
+            <Link key={path} className='text-black text-[15px] uppercase font-semibold
+             cursor-pointer px-3 py-2 dark:text-white rounded-lg hover:bg-red-600 hover:text-white'
+              to={link} spy={true} offset={-100} smooth={true}>
+              {link}
+            </Link>
+          ))
+        }
+      </ul>
+
+      {/* mobile menu icon starts here */}
+      <div className='flex justify-center items-center lg:hidden' onClick={toggleMenu}>
+        <div>
+          {isMenuopen ? <FaCircle className="text-black dark:text-white text-2xl cursor-pointer"/> : <FaBars className="text-black dark:text-white text-2xl cursor-pointer"/>}
+        </div>
+      </div>
+
+      <div className={`${isMenuopen ? 'flex' : 'hidden'} w-full h-fit bg-slate-800 p-4 absolute top-[80px] left-0`} onClick={closeMenu}>
+        <ul className='flex flex-col justify-center items-center gap-2 w-full'>
+          {
+            navitems.map(({link,path})=>(
+              <Link key={path} className='text-white uppercase font-semibold cursor-pointer p-2 rounded-lg hover:bg-red-600 hover:text-black w-full text-center'
+              to={link} spy={true} offset={-100} smooth={true}>
+                {link}
+              </Link>
+            ))
+          }
+        </ul>
+      </div>
+
+      <div className='flex justify-center items-center lg:gap-8 gap-2'>
+        <div className='flex justify-center items-center lg:gap-3 gap-1'>
+          <FaPhoneAlt className='size-5 text-red-600'/>
+          <h1 className='lg:text-xl text-sm text-black dark:text-white font-semibold'>
+            930 536 3432
+          </h1>
+        </div>
+        <FaUserCircle className='size-6 text-red-600 '/>
+      </div>
+    </nav>
   )
 }
-
 export default Header
--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -18,7 +18,7 @@ const Contact = () => {
 
     <>
       <div className={`${darkMode ? "dark: bg-black" : "light bg-transparent"} pb-20`}>
-        <section id="content" className={`${darkMode ? "dark: bg-gray-800" : "light bg-red-100"} lg:w-[95%] w-full m-auto rounded-xl grid lg:grid-cols-2 grid-cols-1 justify-center
+        <section id="contact" className={`${darkMode ? "dark: bg-gray-800" : "light bg-red-100"} lg:w-[95%] w-full m-auto rounded-xl grid lg:grid-cols-2 grid-cols-1 justify-center
       items-center lg:px-36 px-6 py-20 gap-10`}>
           <div data-aos="zoom-in" className='bg-white dark:bg-black p-10 flex flex-col justify-center items-start gap-4 rounded-xl'>
             <h1 className='text-2xl text-black font-semibold dark:text-white'>Send us message today</h1>
